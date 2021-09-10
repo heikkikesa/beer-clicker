5499eb2afda77ecc16ce96ea71df524ae16a3380
Heikki Kesa <heikki.kesa@vincit.fi>
2021-09-10 14:12:50 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import ClickerElement from "./ClickerElement";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: "#345",
+      height: "100%",
+      width: "100%",
+      position: "absolute",
+    },
+  })
+);
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <ClickerElement />
     </div>
   );
 }
