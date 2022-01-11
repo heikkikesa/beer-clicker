2474a10c27a32e79b5c99bbf81ab6f62cb7072e9
Heikki Kesa <heikki.kesa@vincit.fi>
2022-01-11 15:53:47 +0200

    Added Firebase Analytics support

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { initialize } from "./firebase";
 
 const theme = createTheme({
   palette: {
@@ -43,3 +44,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+initialize();
